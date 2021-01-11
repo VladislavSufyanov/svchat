5c8d74b14616d2c6a23cdf643c88f6258c4f24b9
VladislavSufyanov <sufyanovvlad@gmail.com>
2021-01-11 04:20:33 +0300

    Improve message work

--- src_server/main.py
+++ src_server/main.py
@@ -1,29 +1,43 @@
-from fastapi import FastAPI, WebSocket, websockets
+import json
+from functools import lru_cache
+
+from fastapi import FastAPI, WebSocket, Depends
 from fastapi.concurrency import run_until_first_complete
 
 from redis_vsc.redis_vsc import RedisVSC
+from config import config
 
 
 app = FastAPI()
 broadcast_channel = 'anonymity'
 
 
+@lru_cache()
+def get_settings() -> config.Settings:
+    return config.Settings()
+
+
 async def chat_receiver(websocket: WebSocket, redis_client: RedisVSC):
-    async for message in websocket.iter_text():
-        await redis_client.publish(channel=broadcast_channel, message=message)
+    async for message in websocket.iter_json():
+        if isinstance(message, dict):
+            await redis_client.publish(channel=message.get('channel', broadcast_channel),
+                                       message=json.dumps({'from': redis_client.client_id,
+                                                           'message': message['message']}))
 
 
 async def chat_sender(websocket: WebSocket, redis_client: RedisVSC):
-    await redis_client.subscribe(channel=broadcast_channel)
+    await redis_client.subscribe(channel=redis_client.client_id)
     async for dict_message in redis_client:
-        await websocket.send_text(dict_message['message'])
+        await websocket.send_json(dict_message)
 
 
-@app.websocket('/broad_chat/{client_id}')
-async def chat_room(websocket: WebSocket, client_id: str):
+@app.websocket('/secure_chat/{client_id}')
+async def chat_room(websocket: WebSocket, client_id: str, settings: config.Settings = Depends(get_settings)):
     await websocket.accept()
-    redis_client = RedisVSC('localhost', 6379)
+    redis_client = RedisVSC(settings.REDIS_HOST, settings.REDIS_PORT, settings.REDIS_PASSWORD,
+                            settings.REDIS_DB_NUMBER, client_id)
     await redis_client.connect()
+    await redis_client.publish(broadcast_channel, f'Client #{client_id} joined')
     await run_until_first_complete(
         (chat_receiver, {'websocket': websocket, 'redis_client': redis_client}),
         (chat_sender, {'websocket': websocket, 'redis_client': redis_client})
--- src_server/redis_vsc/redis_vsc.py
+++ src_server/redis_vsc/redis_vsc.py
@@ -1,13 +1,18 @@
-import typing
+from typing import Dict, Optional
+import json
 
 import asyncio_redis
+from asyncio_redis import encoders
 
 
 class RedisVSC:
 
-    def __init__(self, host: str, port: int):
+    def __init__(self, host: str, port: int, password: Optional[str], db: Optional[int], client_id: str):
+        self.client_id = client_id
         self._host = host
         self._port = port
+        self._password = password
+        self._db = db
         self._pub_conn = None
         self._sub_conn = None
         self._subscriber = None
@@ -22,8 +27,12 @@ class RedisVSC:
         return self
 
     async def connect(self) -> None:
-        self._pub_conn = await asyncio_redis.Connection.create(self._host, self._port)
-        self._sub_conn = await asyncio_redis.Connection.create(self._host, self._port)
+        self._pub_conn = await asyncio_redis.Connection.create(self._host, self._port,
+                                                               password=self._password,
+                                                               db=self._db)
+        self._sub_conn = await asyncio_redis.Connection.create(self._host, self._port,
+                                                               password=self._password,
+                                                               db=self._db)
         self._subscriber = await self._sub_conn.start_subscribe()
 
     def disconnect(self) -> None:
@@ -39,6 +48,6 @@ class RedisVSC:
     async def publish(self, channel: str, message: str) -> None:
         await self._pub_conn.publish(channel, message)
 
-    async def get_message(self) -> typing.Dict[str, str]:
+    async def get_message(self) -> Dict[str, str]:
         message = await self._subscriber.next_published()
-        return {'channel': message.channel, 'message': message.value}
+        return {'channel': message.channel, 'message': json.loads(message.value)}
